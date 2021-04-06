c6e218030627da7b3f52d919e7739aff7b2516b6
AlanL543 <73874120+AlanL543@users.noreply.github.com>
2021-04-06 12:09:39 +0200

    Add files via upload

---
+++ Exercice_JS_0.8/Mastermind.js
@@ -0,0 +1,21 @@
+let gpions = 0;
+let dpions = 0;
+let hpions = 0;
+let bpions = 0;
+let couleurpions = #FFFFFF;
+let nomjoueurs = '';
+function movepions (){
+	$('#<').click(function(){
+		gpions++;
+	}
+	$('#>').click(function() {
+		dpions++;
+	}
+	$('#^').click(function() {
+		hpions++;
+	}
+	$('#v').click(function() {
+		bpions++;
+	}
+	
+}
